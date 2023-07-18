b4526d2e667d3b26dc52740ce8b44a45fe557553
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-07-18 19:53:26 +1000

    basic apollo server

---
+++ server3-bookly/src/resolvers.ts
@@ -0,0 +1,14 @@
+export const resolvers = {
+    Query: {
+        services: () => [
+            {
+            name: 'mens haircut',
+            price: 20.00,
+            },
+            {
+            name: 'womens haircut',
+            price: 50.00,
+            },
+        ],
+    },
+};
