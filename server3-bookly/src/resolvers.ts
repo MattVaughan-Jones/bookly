5ca3ea5a3b853cb081736792373f0568d8170841
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-07-18 21:23:00 +1000

    removed unused code

--- server3-bookly/src/resolvers.ts
+++ server3-bookly/src/resolvers.ts
@@ -2,18 +2,6 @@ import { Service } from './database/models/serviceModel.js';
 
 export const resolvers = {
     Query: {
-        // services: () => [
-        //     {
-        //     id: '1',
-        //     name: 'mens haircut',
-        //     price: 20.00,
-        //     },
-        //     {
-        //     id: '2',
-        //     name: 'womens haircut',
-        //     price: 50.00,
-        //     },
-        // ],
         services: async () => await Service.findAll()
     },
 };
