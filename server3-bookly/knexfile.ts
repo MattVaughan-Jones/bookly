d5dc75e367779ef62aa61d77057f969349ee65f3
Lucas Reher <l.reher@arkoselabs.com>
2023-07-26 22:26:35 +1000

    fix stuff

---
+++ server3-bookly/knexfile.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+export default {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    }
+  },
+};
