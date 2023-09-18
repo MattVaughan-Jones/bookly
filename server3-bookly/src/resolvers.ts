a152b49fc82fdfa60a24f186a1a1dc5a8f8e923b
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-09-18 15:50:46 +1000

    removed references to getOrganisationUsers

--- server3-bookly/src/resolvers.ts
+++ server3-bookly/src/resolvers.ts
@@ -2,7 +2,6 @@ import { getUsers } from './queries/getUsers.js';
 import { getUser } from './queries/getUser.js';
 import { getOrganisations } from './queries/getOrganisations.js';
 import { getOrganisation } from './queries/getOrganisation.js';
-import { getOrganisationUsers } from './queries/getOrganisationUsers.js';
 
 
 export const resolvers = {
@@ -17,7 +16,6 @@ export const resolvers = {
         organisation(parent, args) { return getOrganisation(parent, args) },
         users: getUsers,
         user(parent, args) { return getUser(parent, args) },
-        organisationUsers(parent, args) { getOrganisationUsers(parent, args) },
         // userOrganisations: async () => await, 
     },
 };
