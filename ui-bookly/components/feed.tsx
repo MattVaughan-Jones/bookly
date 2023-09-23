21a2ce0c58ad301a298a5eac6fe2b23566b847e4
MattVaughan-Jones <71620998+MattVaughan-Jones@users.noreply.github.com>
2023-09-23 14:46:17 +1000

    statefull rendering of organisations in Feed component

--- ui-bookly/components/feed.tsx
+++ ui-bookly/components/feed.tsx
@@ -5,39 +5,43 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { OrganisationCard } from './OrganisationCard';
 
-const OrganisationCardList = () => {
-    const organisations: {id: number, name: string}[] = [
-        {
-            id: 1,
-            name: 'org 1',
-        },
-        {
-            id: 2,
-            name: 'org 2',
-        },
-        {
-            id: 3,
-            name: 'org 3',
-        }
-    ];
-
-    return (
-        <>
-            <Grid sx={{pt: 2}} container spacing={2}>
-                {organisations.map((organisation: any) => {
-                    return (
-                        <OrganisationCard organisation={organisation} key={organisation.id}/>
-                    )
-                })}    
-            </Grid>
-        </>
-    )
-};
-
 const Feed: () => React.ReactElement = () => {
 
+    const OrganisationCardList = () => {
+        return (
+            <>
+                <Grid sx={{pt: 2}} container spacing={2}>
+                    {organisations.map((organisation: any) => {
+                        return (
+                            <OrganisationCard organisation={organisation} key={organisation.id}/>
+                        )
+                    })}    
+                </Grid>
+            </>
+        )
+    };
+
+    useEffect(() => {
+        const data: {id: number, name: string}[] = [
+            {
+                id: 1,
+                name: 'org 1',
+            },
+            {
+                id: 2,
+                name: 'org 2',
+            },
+            {
+                id: 3,
+                name: 'org 3',
+            }
+        ];
+
+        setOrganisations(data);
+    })
+
     const [searchText, setSearchText] = useState('');
-    const [organisations, setOrganisations] = useState([]);
+    const [organisations, setOrganisations] = useState([{}]);
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
